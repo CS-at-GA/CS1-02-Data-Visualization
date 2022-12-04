add12e27e2d88306c625ad886f8eef8934a7b253
Mr. Oswald <62300716+gajoswald@users.noreply.github.com>
2022-12-04 20:00:04 +0000

    created basic structure

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+let data;
+
+function preload() {
+  // remember to load your data here using functions like
+  // loadJSON - https://p5js.org/reference/#/p5/loadJSON
+  // loadTable - https://p5js.org/reference/#/p5/loadTable
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
+  noLoop();
 }
 
 function draw() {
-  noStroke()
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+  
+}
+
+function keyPressed() {
+  // recall that you might want to combine or trim your
+  // data source(s) down a bit. In that case, it might 
+  // be useful to use functions like:
+  // saveJSON - https://p5js.org/reference/#/p5/saveJSON
+  // saveTable - https://p5js.org/reference/#/p5/saveTable
 }
